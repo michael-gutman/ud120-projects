db2d2eb10359990d731c448b1f57a2ea8fdc0c11
michael-gutman <michaelgutman@hotmail.com>
2017-05-21 19:00:22 -0400

    timing my tests

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -29,7 +29,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
+t1 = time()
 pred = clf.predict(features_test)
+print("predicting time:", round(time()-t1, 3), "s")
 print(accuracy_score(pred, labels_test))
 #########################################################
